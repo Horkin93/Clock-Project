da64c9cb18c0e9cb027f44006766d852399f7b50
AKhorkin <horkin93@gmail.com>
2022-06-17 21:10:36 +0300

    Add Landscape Solution

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using TMPro;
@@ -11,8 +10,15 @@ public class Clock : MonoBehaviour
     [SerializeField] private GameObject _hours;
     [SerializeField] private GameObject _minutes;
     [SerializeField] private GameObject _seconds;
+    [Header("Digital Clock")]
     [SerializeField] private TextMeshProUGUI _digitalClock;
     private DateTime _currentTime;
+
+    private enum Provider
+    {
+        WorldTimeApi,
+        UnixTime
+    }
 
     private struct WorldTimeApi
     {
@@ -20,9 +26,14 @@ public class Clock : MonoBehaviour
         public double unixtime;
     }
 
+    private struct UnixTime
+    {
+        public double UnixTimeStamp;
+    }
+
     private void Awake()
     {
-        StartCoroutine(SetTimeFromServer("http://worldtimeapi.org/api/ip"));
+        StartCoroutine(SetTimeFromServer("http://worldtimeapi.org/api/ip", Provider.WorldTimeApi));
     }
 
     private void Start()
@@ -31,6 +42,20 @@ public class Clock : MonoBehaviour
     }
     private void Update()
     {
+        if (Screen.orientation == ScreenOrientation.LandscapeLeft)
+        {
+            Debug.Log(ScreenOrientation.LandscapeLeft);
+            _digitalClock.transform.localPosition = new Vector3(-700, 0);
+        }
+        else if(Screen.orientation == ScreenOrientation.LandscapeRight)
+        {
+            Debug.Log(ScreenOrientation.LandscapeRight);
+            _digitalClock.transform.localPosition = new Vector3(700, 0);
+        }
+        else
+        {
+            _digitalClock.transform.localPosition = new Vector3(0, 600);
+        }
         _currentTime = _currentTime.AddSeconds(Time.deltaTime);
         SetTime(_currentTime);
     }
@@ -51,7 +76,7 @@ public class Clock : MonoBehaviour
         _hours.transform.localEulerAngles = new Vector3(0, 0, -hours/12*360);
         if (_hours.transform.localEulerAngles.z == 0.0f)
         {
-            StartCoroutine(SetTimeFromServer("http://worldtimeapi.org/api/ip"));
+            StartCoroutine(SetTimeFromServer("https://showcase.api.linx.twenty57.net/UnixTime/tounixtimestamp?datetime=now", Provider.UnixTime));
         }
     }
 
@@ -60,7 +85,7 @@ public class Clock : MonoBehaviour
         _digitalClock.text = time.ToString("HH") + ":" + time.ToString("mm") + ":" + time.ToString("ss");
     }
 
-    private IEnumerator SetTimeFromServer(string url)
+    private IEnumerator SetTimeFromServer(string url, Provider provider)
     {
         UnityWebRequest webRequest = UnityWebRequest.Get(url);
         webRequest.timeout = 3;
@@ -73,9 +98,20 @@ public class Clock : MonoBehaviour
         }
         else
         {
-            Debug.Log("Time : " + webRequest.downloadHandler.text);
-            WorldTimeApi worldTimeApi = JsonUtility.FromJson<WorldTimeApi>(webRequest.downloadHandler.text);
-            _currentTime = new DateTime(1970, 1, 1, 3, 0, 0, 0).AddSeconds(worldTimeApi.unixtime);
+            switch (provider)
+            {
+                case Provider.WorldTimeApi:
+                    Debug.Log("Time : " + webRequest.downloadHandler.text);
+                    WorldTimeApi worldTimeApi = JsonUtility.FromJson<WorldTimeApi>(webRequest.downloadHandler.text);
+                    _currentTime = new DateTime(1970, 1, 1, 3, 0, 0, 0).AddSeconds(worldTimeApi.unixtime);
+                    break;
+                case Provider.UnixTime:
+                    Debug.Log("Time : " + webRequest.downloadHandler.text);
+                    UnixTime unixTime = JsonUtility.FromJson<UnixTime>(webRequest.downloadHandler.text);
+                    _currentTime = new DateTime(1970, 1, 1, 3, 0, 0, 0).AddSeconds(unixTime.UnixTimeStamp);
+                    break;
+            }
+            
         }
 
     }
